be0e0449cec511ba1c951e26eb197a67a6df8293
webdog369 <1136116938@qq.com>
2020-08-22 23:25:17 +0800

    完成顶栏及其换肤功能

--- music-item/src/App.vue
+++ music-item/src/App.vue
@@ -1,8 +1,25 @@
 <template>
   <div id="app">
-
+    <Header></Header>
   </div>
 </template>
-
+<script>
+import Header from './components/Header'
+export default {
+  name: 'App',
+  components: {
+    Header
+  },
+  methods: {
+  }
+}
+</script>
 <style lang="scss">
+  @import 'assets/css/variable';
+  #app{
+    position: relative;
+    width: 100%;
+    height: 100%;
+  }
+
 </style>
---
+++ music-item/src/components/Header.vue
@@ -0,0 +1,61 @@
+<template>
+<div class="header" @click="changeTheme">
+  <div class="header-left"></div>
+  <p class="header-title" ref="headerTitle">网易云音乐</p>
+  <div class="header-right"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      theme: [['theme', '网易云音乐'], ['theme1', 'QQ音乐'], ['theme2', '知渔音乐']],
+      num: 0
+    }
+  },
+  methods: {
+    changeTheme () {
+      this.num++
+      if (this.num > 2) {
+        this.num = 0
+      }
+      // 切换主题
+      document.documentElement.setAttribute('data-theme', this.theme[this.num][0])
+      this.$refs.headerTitle.innerHTML = this.theme[this.num][1]
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  @import "src/assets/css/variable";
+  @import "src/assets/css/mixin";
+  .header{
+    width: 100%;
+    height: 100px;
+    @include bg_color();
+    display: flex;
+    justify-content: space-between;
+    .header-left,.header-right{
+      width: 84px;
+      height: 84px;
+      margin-top: 8px;
+    }
+    .header-left{
+      @include bg_img('../assets/images/logo')
+    }
+    .header-right{
+      @include bg_img('../assets/images/account')
+    }
+    .header-title{
+      text-align: center;
+      line-height: 100px;
+      color:#fff;
+      font-weight: bold;
+      @include font_size($font_medium)
+    }
+  }
+
+</style>
