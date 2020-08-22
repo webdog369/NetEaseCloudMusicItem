be38ac5d2d2e560666d8998926097e9aa8f80c4b
webdog369 <1136116938@qq.com>
2020-08-23 00:01:06 +0800

    完成tabbar选项卡搭建

---
+++ music-item/src/components/Tabbar.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="tabbar">
+      <router-link tag="div" class="item" to="/recommend">
+        <span>推荐</span>
+      </router-link>
+      <router-link tag="div" class="item" to="/singer">
+        <span>歌手</span>
+      </router-link>
+      <router-link tag="div" class="item" to="/list">
+        <span>排行</span>
+      </router-link>
+      <router-link tag="div" class="item" to="/search">
+        <span>搜索</span>
+      </router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Tabbar'
+}
+</script>
+
+<style scoped lang="scss">
+  @import "src/assets/css/variable";
+  @import "src/assets/css/mixin";
+.tabbar{
+  width: 100%;
+  height: 84px;
+  @include bg_sub_color();
+  display: flex;
+  justify-content: space-around;
+  .item{
+    text-align: center;
+    line-height: 84px;
+    span{
+      @include font_size($font_medium);
+      @include font_color();
+    }
+    &.router-link-active{
+      border-bottom-width:5px;
+      border-bottom-style:solid;
+      @include border_color();
+      span{
+        @include font_active_color();
+      }
+    }
+  }
+}
+
+</style>
